a0d7f3ba2ccd7c806a94b49955528e5cf00295be
oiceo123 <iceome44@gmail.com>
2023-09-24 00:17:30 +0700

    init environment

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/oiceo123/kawaii-shop-tutorial/config"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
 func main() {
-	
+	cfg := config.LoadConfig(envPath())
+	fmt.Println(cfg.App())
+	fmt.Println(cfg.Db())
+	fmt.Println(cfg.Jwt())
 }
